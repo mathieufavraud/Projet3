ffad6e94dd2c99906e8a8e32eb191a20e4f736da
mathieufavraud <131956332+mathieufavraud@users.noreply.github.com>
2023-05-27 18:02:11 +0200

    Update login.js

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -17,30 +17,22 @@ async function CheckLogin(email, pwd) // Vérification login
 
 async function CheckInputs() // Vérification valeur saisies
 {
-    let email = document.getElementById('email').value.trim();
-    let pwd = document.getElementById('pwd').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const pwd = document.getElementById('pwd').value.trim();
     
-    if(email=='')
-    {
-        alert("Mail non valide");
-    }
-    else if (pwd=='')
-    {
-        alert("Mot de passe non valide")
-    }
-    else 
+
+    if(email !== "" && pwd !== "")
     {
         let login = await CheckLogin(email, pwd);
 
         if(login.userId==1)
         {
-            alert("Bienvenue Sophie !"); // peut être supprimé
-            window.sessionStorage.setItem("token", `${login.token}`); // test avec sessionstorage
+            window.sessionStorage.setItem("token", `${login.token}`); 
             window.location.href='index.html';
         }
         else
         {
-            alert("Email ou mot de passe invalide");
+            alert("Erreur dans l’identifiant ou le mot de passe");
         }
     }
 }
@@ -49,4 +41,4 @@ document.getElementById('connect').addEventListener("click", (event) => {
     event.preventDefault();
     event.stopPropagation();
     CheckInputs();
-}) // Gestion de l'évènement click sur le bouton de connection
+}) 
